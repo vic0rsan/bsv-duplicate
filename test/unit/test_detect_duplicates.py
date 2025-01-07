1ba0900bb7bf3192ed618ec89de4322394a25157
vic0r <6764211-h4xx0rg@users.noreply.gitlab.com>
2025-01-07 12:59:43 +0100

    commit

--- test/unit/test_detect_duplicates.py
+++ test/unit/test_detect_duplicates.py
@@ -1,29 +1,59 @@
 import pytest
 from unittest.mock import MagicMock
 from src.util.detector import detect_duplicates
+#from src.util.parser import parse
+from unittest.mock import patch
+
+DATA_DUP_KEY_DOI = """@article{SAME,
+  title={Naming the pain in requirements engineering: Contemporary Problems, Causes, and Effects in Practice},
+  doi={10.1007/s10664-016-9451-7}
+}
+
+@article{SAME,
+  title={Naming the pain in requirements engineering: Contemporary Problems, Causes, and Effects in Practice},
+  doi={10.1007/s10664-016-9451-7}
+}"""
+
+DATA_DUP_KEY = """@article{SAME,
+  title={Naming the pain in requirements engineering: Contemporary Problems, Causes, and Effects in Practice},
+}
+
+@article{SAME,
+  title={Naming the pain in requirements engineering: Contemporary Problems, Causes, and Effects in Practice},
+}"""
+
+DATA_UNIQ = """@article{SAME,
+  title={Naming the pain in requirements engineering: Contemporary Problems, Causes, and Effects in Practice},
+}
+
+@article{NOT SAME,
+  title={Naming the pain in requirements engineering: Contemporary Problems, Causes, and Effects in Practice},
+}"""
 
 # develop your test cases here
+
 
 @pytest.mark.unit
 def test_detect_duplicates():
     assert True
 
-@pytest.fixture
-def article_mock():
-    mock_dao = MagicMock()
-    return mock_dao
-
 @pytest.mark.unit
 def test_len_0():
-    data = []
     with pytest.raises(ValueError, match="The input data does not contain enough articles to detect duplicates."):
-        detect_duplicates(data)
-   
+        detect_duplicates("")
 
 @pytest.mark.unit
-def test_article_same_name_doi(article_mock):
-    data = ["frattini2023requirements", "fernandez2017naming", "mendez2017naming", "wagner2019status", "frattini2023requirements"]
-    article_mock.parse = MagicMock(return_value=[data])
-    detect_duplicates(data)
+def test_article_same_doi_key():
+    res = detect_duplicates(DATA_DUP_KEY_DOI)
+    assert len(res) == 1
     
+@pytest.mark.unit
+def test_article_same_key_no_doi():
+    res = detect_duplicates(DATA_DUP_KEY)
+    assert len(res) == 1
 
+@pytest.mark.unit
+def test_article_no_duplicate():
+    res = detect_duplicates(DATA_UNIQ)
+    assert len(res) == 0
+    
